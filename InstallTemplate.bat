aff3ab7fda261d2a45bea7b017854a10401ec4c4
gmh5225 <2315157@qq.com>
2022-03-19 20:51:26 +0800

    First commit

---
+++ InstallTemplate.bat
@@ -0,0 +1,120 @@
+@ECHO OFF
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+SET "EXIT_ON_ERROR=%~1"
+SET SUCCESS=0
+
+PUSHD %~dp0
+
+SET VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+
+@rem Visual Studio 2015
+SET VCT_PATH=%ProgramFiles(x86)%\MSBuild\Microsoft.Cpp\v4.0\v140\Platforms
+IF EXIST "%VCT_PATH%" CALL :SUB_VS2015
+SET VCT_PATH=%ProgramFiles%\MSBuild\Microsoft.Cpp\v4.0\v140\Platforms
+IF EXIST "%VCT_PATH%" CALL :SUB_VS2015
+
+@rem Visual Studio 2017
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [15.0^,16.0^)') DO (
+	SET VCT_PATH=%%A\Common7\IDE\VC\VCTargets\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!"
+)
+
+@rem Visual Studio 2017 Build Tools
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -products Microsoft.VisualStudio.Product.BuildTools -property installationPath -prerelease -version [15.0^,16.0^)') DO (
+	SET VCT_PATH=%%A\Common7\IDE\VC\VCTargets\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!"
+)
+
+@rem Visual Studio 2019, 2022
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -property installationPath -prerelease -version [16.0^,18.0^)') DO (
+	@rem Visual C++ 2022 v143 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v170\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2022 "!VCT_PATH!"
+	@rem Visual C++ 2019 v142 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v160\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2019 "!VCT_PATH!"
+	@rem Visual C++ 2017 v141 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v150\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!"
+)
+
+@rem Visual Studio 2019, 2022 Build Tools
+FOR /f "delims=" %%A IN ('"%VSWHERE%" -products Microsoft.VisualStudio.Product.BuildTools -property installationPath -prerelease -version [16.0^,18.0^)') DO (
+	@rem Visual C++ 2022 v143 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v170\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2022 "!VCT_PATH!"
+	@rem Visual C++ 2019 v142 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v160\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2019 "!VCT_PATH!"
+	@rem Visual C++ 2017 v141 toolset
+	SET VCT_PATH=%%A\MSBuild\Microsoft\VC\v150\Platforms
+	IF EXIST "!VCT_PATH!" CALL :SUB_VS2017 "!VCT_PATH!"
+)
+
+IF %SUCCESS% == 0 (
+	ECHO Visual C++ 2017, 2019 or 2022 NOT Installed.
+	IF "%EXIT_ON_ERROR%" == "" EXIT /B
+)
+
+POPD
+ENDLOCAL
+EXIT /B
+
+
+:SUB_VS2015
+ECHO VCTargetsPath for Visual Studio 2015: %VCT_PATH%
+XCOPY /Q /Y "LLVM-MSVC" "%VCT_PATH%\..\LLVM-MSVC\"
+XCOPY /Q /Y "LLVM-MSVC_v140" "%VCT_PATH%\x64\PlatformToolsets\LLVM-MSVC_v140\"
+XCOPY /Q /Y "LLVM-MSVC_v140_xp" "%VCT_PATH%\x64\PlatformToolsets\LLVM-MSVC_v140_xp\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\x64\PlatformToolsets\LLVM-MSVC_v140_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v140" "%VCT_PATH%\Win32\PlatformToolsets\LLVM-MSVC_v140\"
+XCOPY /Q /Y "LLVM-MSVC_v140_xp" "%VCT_PATH%\Win32\PlatformToolsets\LLVM-MSVC_v140_xp\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v140_KernelMode\"
+SET SUCCESS=1
+
+
+:SUB_VS2017
+ECHO VCTargetsPath for Visual Studio 2017: %~1
+XCOPY /Q /Y "LLVM-MSVC" "%~1\..\LLVM-MSVC\"
+XCOPY /Q /Y "LLVM-MSVC_v141" "%~1\x64\PlatformToolsets\LLVM-MSVC_v141\"
+XCOPY /Q /Y "LLVM-MSVC_v141_xp" "%~1\x64\PlatformToolsets\LLVM-MSVC_v141_xp\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\x64\PlatformToolsets\LLVM-MSVC_v141_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v141" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v141\"
+XCOPY /Q /Y "LLVM-MSVC_v141_xp" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v141_xp\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v141_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v141" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v141\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v141_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v141" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v141\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v141_KernelMode\"
+SET SUCCESS=1
+EXIT /B
+
+:SUB_VS2019
+ECHO VCTargetsPath for Visual Studio 2019: %~1
+XCOPY /Q /Y "LLVM-MSVC" "%~1\..\LLVM-MSVC\"
+XCOPY /Q /Y "LLVM-MSVC_v142" "%~1\x64\PlatformToolsets\LLVM-MSVC_v142\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\x64\PlatformToolsets\LLVM-MSVC_v142_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v142" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v142\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v142_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v142" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v142\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v142_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v142" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v142\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v142_KernelMode\"
+SET SUCCESS=1
+XCOPY /Q /Y "ProjectTemplates" "%USERPROFILE%\Documents\Visual Studio 2019\Templates\ProjectTemplates\"
+EXIT /B
+
+:SUB_VS2022
+ECHO VCTargetsPath for Visual Studio 2022: %~1
+XCOPY /Q /Y "LLVM-MSVC" "%~1\..\LLVM-MSVC\"
+XCOPY /Q /Y "LLVM-MSVC_v143" "%~1\x64\PlatformToolsets\LLVM-MSVC_v143\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\x64\PlatformToolsets\LLVM-MSVC_v143_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v143" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v143\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\Win32\PlatformToolsets\LLVM-MSVC_v143_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v143" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v143\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM64\PlatformToolsets\LLVM-MSVC_v143_KernelMode\"
+XCOPY /Q /Y "LLVM-MSVC_v143" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v143\"
+XCOPY /Q /Y "LLVM-MSVC_KernelMode" "%~1\ARM\PlatformToolsets\LLVM-MSVC_v143_KernelMode\"
+SET SUCCESS=1
+EXIT /B
---
+++ LLVM-MSVC setup.iss
@@ -0,0 +1,82 @@
+#dim Version[4]
+#expr ParseVersion("bin\clang-cl.exe", Version[0], Version[1], Version[2], Version[3])
+#define MyAppVersion Str(Version[0]) + "." + Str(Version[1]) + "." + Str(Version[2]) + "." + Str(Version[3])
+#define MyAppName "llvm-msvc"
+#define MyAppExeName "llvm-msvc_X86_64.exe"
+
+[Setup]
+AppName={#MyAppName}
+AppVersion={#MyAppVersion}
+AppId={#MyAppName}
+DefaultDirName="{src}\llvm-msvc_X86_64"
+Compression=lzma2
+SolidCompression=yes
+DisableReadyPage=no
+DisableReadyMemo=no
+DisableStartupPrompt=yes
+DisableFinishedPage=yes
+Uninstallable=yes
+OutputDir=Output\
+OutputBaseFilename=llvm-msvc_X86_64
+PrivilegesRequired=admin
+
+
+
+[Files]
+
+;bin
+Source: "bin\*.*"; DestDir: "{app}\bin\"; Flags: ignoreversion
+
+;lib
+Source: "lib\*"; DestDir: "{app}\lib"; Flags: ignoreversion recursesubdirs
+
+;doc
+Source: "doc\*"; DestDir: "{app}\doc"; Flags: ignoreversion recursesubdirs
+
+
+
+;template
+Source: "InstallTemplate.bat"; DestDir: "{app}\template\"; Flags: ignoreversion
+Source: "UnInstallTemplate.bat"; DestDir: "{app}\template\"; Flags: ignoreversion
+Source: "LLVM-MSVC\*"; DestDir: "{app}\template\LLVM-MSVC"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v140\*"; DestDir: "{app}\template\LLVM-MSVC_v140"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v140_xp\*"; DestDir: "{app}\template\LLVM-MSVC_v140_xp"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v141\*"; DestDir: "{app}\template\LLVM-MSVC_v141"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v141_xp\*"; DestDir: "{app}\template\LLVM-MSVC_v141_xp"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v142\*"; DestDir: "{app}\template\LLVM-MSVC_v142"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_v143\*"; DestDir: "{app}\template\LLVM-MSVC_v143"; Flags: ignoreversion recursesubdirs
+Source: "LLVM-MSVC_KernelMode\*"; DestDir: "{app}\template\LLVM-MSVC_KernelMode"; Flags: ignoreversion recursesubdirs
+
+[Languages]
+Name: "en"; MessagesFile: "compiler:Default.isl"
+Name: "tc"; MessagesFile: "compiler:Languages\ChineseTraditional.isl"
+Name: "jp"; MessagesFile: "compiler:Languages\Japanese.isl"
+Name: "de"; MessagesFile: "compiler:Languages\German.isl"
+
+[Registry]
+ Root: HKLM; Subkey: "SOFTWARE\LLVM\LLVM-MSVC"; ValueType: "string"; ValueData: "{app}"
+
+[Run]
+Filename: {app}\template\InstallTemplate.bat; Flags: shellexec runhidden nowait; 
+
+[Code]
+procedure CurUninstallStepChanged(CurUninstallStep: TUninstallStep);
+var
+   ReturnCode:   Integer;
+begin
+  if CurUninstallStep = usUninstall then begin
+    Exec(ExpandConstant('{app}\template\UnInstallTemplate.bat'), '', '', SW_HIDE, ewWaitUntilTerminated, ReturnCode);
+    RegDeleteKeyIncludingSubkeys(HKEY_LOCAL_MACHINE, 'Software\LLVM\LLVM-MSVC');
+
+    DelTree(ExpandConstant('{app}\bin'), True, True, True);
+    DelTree(ExpandConstant('{app}\lib'), True, True, True);
+    DelTree(ExpandConstant('{app}\doc'), True, True, True);
+
+  end;
+end;
+
+function InitializeSetup(): Boolean;
+begin
+
+	Result := true;
+end;
